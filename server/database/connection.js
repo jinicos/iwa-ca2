6e4a61162fc0e8aca5513f7b0028c90e9c4cbd4a
jinicos <2018391@student.cct.ie>
2022-07-31 21:34:23 +0500

    Change in design and table

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection string
-        // const con = await mongoose.connect(process.env.MONGO_URI, {
-        //     useNewUrlParser: true,
-        //     useUnifiedTopology: true,
-        //     useFindAndModify: false,
-        //     useCreateIndex: true
-        // })
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
 
         console.log(`MongoDB connected : ${con.connection.host}`);
     }catch(err){
