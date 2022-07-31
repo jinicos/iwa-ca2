8b432968e0d419f2d95fea61b10f9dad53b0101d
jinicos <2018391@student.cct.ie>
2022-07-31 21:23:47 +0500

    controller.js changing

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -9,12 +9,12 @@ exports.create = (req,res)=>{
     }
 
     // new user
-    const user = new Userdb({
-        name : req.body.name,
-        email : req.body.email,
-        gender: req.body.gender,
-        status : req.body.status
-    })
+    // const user = new Userdb({
+    //     name : req.body.name,
+    //     email : req.body.email,
+    //     gender: req.body.gender,
+    //     status : req.body.status
+    // })
 
     // save user in the database
     user
